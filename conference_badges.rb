608e8bbf1e54e4fdf4488a267b70001090e63a2f
Mikkel Ridley <mikkel250@gmail.com>
2018-09-07 21:53:03 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+def badge_maker(name)
+  
+end
+
+def batch_badge_creator(array)
+  
+end
+
+def assign_rooms(list_of_speakers)
+  "Hello, #{name}! You'll be assigned to room #{room_number}!"
+end
+
+def printer
+  
+end
