8fdf88638c3f698255507618154e2cf020ca6f9e
Mikkel Ridley <mikkel250@gmail.com>
2018-09-07 22:12:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(list_of_speakers)
-  room_assignments = list_of_speakers.map.with_index { |name, room_number| "Hello, #{name}! You'll be assigned to room #{room_number}!"}
+  room_assignments = list_of_speakers.map.with_index { |name, room_number| "Hello, #{name}! You'll be assigned to room #{room_number + 1}!"}
 end
 
 def printer
